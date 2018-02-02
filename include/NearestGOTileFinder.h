9eb2fd9776fac7ad4191c35602608e235541fe79
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2018-02-02 15:30:20 -0200

    Corrigido alguns erros de compilação

--- include/NearestGOTileFinder.h
+++ include/NearestGOTileFinder.h
@@ -3,11 +3,13 @@
 #include "GameObject.h"
 #include "NearestFinder.h"
 
+using namespace RattletrapEngine;
+
 class NearestGOTileFinder : public NearestFinder<GameObject>, public Finder<GameObject>
 {
-public:
-    NearestGOTileFinder();
-    float operator()(GameObject);
+	public:
+		NearestGOTileFinder();
+		float operator()(GameObject);
 };
 
 #endif // NESRESTGOTILEFINDER_H
