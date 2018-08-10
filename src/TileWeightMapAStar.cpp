962cf3d89c93fc0e9b35910d915f9639f1b8b204
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2018-08-10 13:20:29 -0300

    Trocado compilador de volta para o g++ e deixado mais legível a verificação de se um title pode ser andável

--- src/TileWeightMapAStar.cpp
+++ src/TileWeightMapAStar.cpp
@@ -10,7 +10,10 @@ float TileWeightMapAStar::CalculateCost(Tile& tile){
     return weightMap[tile.GetTileSetIndex()];
 }
 bool TileWeightMapAStar::IsTraversable(Tile& tile){
-    return(tile.GetGO() == nullptr && tile.GetTileSetIndex() != END_POINT && tile.GetTileSetIndex() != SPAWN_POINT &&tile.GetTileSetIndex() != TILE_VAZIO);
+    return(tile.GetGO() == nullptr &&
+			tile.GetTileSetIndex() != END_POINT &&
+			tile.GetTileSetIndex() != SPAWN_POINT &&
+			tile.GetTileSetIndex() != TILE_VAZIO);
 }
 
 TileWeightMapAStar::~TileWeightMapAStar(void){
